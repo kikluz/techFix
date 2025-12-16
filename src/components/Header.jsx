8982b191ae88e049dc5396ca428fa3907a1d492e
kikluz <kikluzr@gmail.com>
2025-12-16 11:48:29 -0800

    remove on mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { FaPhone, FaComment, FaClock, FaMapMarkerAlt } from "react-icons/fa";
 // ? Create a header and navigation.
 const Header = () => {
   return (
-    <header className="bg-primary text-primary-content py-2">
+    <header className="bg-primary text-primary-content py-2 hidden md:block">
       <div className="align-element flex flex-col sm:flex-row justify-between items-center text-sm">
         <div className="flex items-center gap-4 mb-2 sm:mb-0">
           <div className="flex items-center gap-1">
